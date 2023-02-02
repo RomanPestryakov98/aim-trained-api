480cb55d42e6adb5a2d81c0d7090820e560030b5
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2023-02-02 19:08:51 +0300

    update router

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const { createUser, login } = require('../controllers/users');
+const { getAllGames, createGame } = require('../controllers/games');
 const auth = require('../middlewares/auth');
 const userRouter = require('./users');
-const gameRouter = require('./games');
 const { validationSignin, validationSignup } = require('../middlewares/validation');
 
 router.post('/signup', validationSignup, createUser);
 router.post('/signin', validationSignin, login);
 
 router.use(auth, userRouter);
-router.use(auth, gameRouter);
+
+router.get('/game/:time', getAllGames)
+router.post('/game', auth, createGame);
 
 module.exports = router;
