60608df67c1c76a757b74e1e74cad6d84a7ce5b5
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2023-02-02 19:17:17 +0300

    udpateee

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 const router = require('express').Router();
 const { createUser, login } = require('../controllers/users');
-const { getAllGames, createGame } = require('../controllers/games');
 const auth = require('../middlewares/auth');
 const userRouter = require('./users');
+const gameRouter = require('./games');
 const { validationSignin, validationSignup } = require('../middlewares/validation');
 
 router.post('/signup', validationSignup, createUser);
 router.post('/signin', validationSignin, login);
 
 router.use(auth, userRouter);
-
-router.get('/game/:time', getAllGames)
-router.post('/game', auth, createGame);
+router.use(gameRouter);
 
 module.exports = router;
